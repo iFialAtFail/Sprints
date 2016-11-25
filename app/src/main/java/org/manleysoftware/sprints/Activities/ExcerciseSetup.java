abe73df718254558809d338f8904661d20b0a972
Michael Manley <mmanle01@gmail.com>
2016-11-25 13:55:06 -0500

    Doing really bad on this commit thing. Anyway, working main timer and count down. Debating on what I should do for the 30/60 framework

--- app/src/main/java/org/manleysoftware/sprints/Activities/ExcerciseSetup.java
+++ app/src/main/java/org/manleysoftware/sprints/Activities/ExcerciseSetup.java
@@ -7,8 +7,11 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import org.manleysoftware.sprints.R;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Michael on 11/24/2016.
@@ -16,12 +19,20 @@ import org.manleysoftware.sprints.R;
 
 public class ExcerciseSetup extends AppCompatActivity {
 
+	public static final String TIME_IN_MILIS = "TimeInMillis";
+
 	Context context = this;
+	EditText editTextHours;
+	EditText editTextMinutes;
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_excercise_setup);
+
+		editTextHours = (EditText) findViewById(R.id.editTextHours);
+		editTextMinutes = (EditText) findViewById(R.id.editTextMinutes);
+
 
 		Button btnGoStartRun = (Button)findViewById(R.id.btnGoStartRun);
 		if (btnGoStartRun != null){
@@ -29,9 +40,20 @@ public class ExcerciseSetup extends AppCompatActivity {
 				@Override
 				public void onClick(View view) {
 					Intent intent = new Intent(context, SprintsRuntime.class);
+					intent.putExtra(TIME_IN_MILIS,tryParseTime());
 					startActivity(intent);
 				}
 			});
+		}
+	}
+
+	private long tryParseTime(){
+		if (editTextHours != null && editTextMinutes != null){
+			long hoursInMillis = TimeUnit.HOURS.toMillis(Long.parseLong(editTextHours.getText().toString()));
+			long minutesInMillis = TimeUnit.MINUTES.toMillis(Long.parseLong(editTextMinutes.getText().toString()));
+			return hoursInMillis + minutesInMillis;
+		} else{
+			return 0;
 		}
 	}
 }
--- app/src/main/java/org/manleysoftware/sprints/Activities/SprintsRuntime.java
+++ app/src/main/java/org/manleysoftware/sprints/Activities/SprintsRuntime.java
@@ -3,12 +3,17 @@ package org.manleysoftware.sprints.Activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.manleysoftware.sprints.R;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Michael on 11/24/2016.
@@ -17,11 +22,40 @@ import org.manleysoftware.sprints.R;
 public class SprintsRuntime extends AppCompatActivity {
 
 	Context context = this;
+	Intent intent;
+	CountDownTimer shortTimer;
+	CountDownTimer longTimer;
+
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_runtime);
+		intent = getIntent();
+
+		shortTimer = new CountDownTimer(TimeUnit.SECONDS.toMillis(30),0) {
+			@Override
+			public void onTick(long l) {
+
+			}
+
+			@Override
+			public void onFinish() {
+				Toast.makeText(context,"30 seconds up",Toast.LENGTH_LONG).show();
+			}
+		};
+
+		longTimer = new CountDownTimer(TimeUnit.SECONDS.toMillis(60),0) {
+			@Override
+			public void onTick(long l) {
+
+			}
+
+			@Override
+			public void onFinish() {
+				Toast.makeText(context,"60 seconds up",Toast.LENGTH_LONG).show();
+			}
+		};
 
 		Button btnEndRun = (Button) findViewById(R.id.btnEndRun);
 		if (btnEndRun != null){
@@ -33,5 +67,23 @@ public class SprintsRuntime extends AppCompatActivity {
 				}
 			});
 		}
+
+		final TextView tvElapsedTime = (TextView) findViewById(R.id.tvOverallTimeElapsed);
+		if (tvElapsedTime != null){
+			//String time = String.valueOf(intent.getLongExtra(ExcerciseSetup.TIME_IN_MILIS, 0));
+			//tvElapsedTime.setText(time);
+		}
+
+		CountDownTimer mainTimer = new CountDownTimer(intent.getLongExtra(ExcerciseSetup.TIME_IN_MILIS,0), 100 ) {
+			@Override
+			public void onTick(long l) {
+				tvElapsedTime.setText("Seconds remaining: " + l / 1000);
+			}
+
+			@Override
+			public void onFinish() {
+				tvElapsedTime.setText("Finished");
+			}
+		}.start();
 	}
 }
